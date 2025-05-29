8a68472a7b24f8ec5de171dad703d4e5d61c7b38
Mohammed <mohammedemad557@yahoo.com>
2025-05-29 11:09:14 +0300

    hello iam here

--- bin/recursion.dart
+++ bin/recursion.dart
@@ -3,5 +3,20 @@ import 'package:args/args.dart';
 const String version = '0.0.1';
 
 void main(List<String> arguments) {
+  // recursive function
 
+  // 1- base case
+  //2- recursive fun ==> loop
+
+  fibonacci(10);
+
+}
+void fibonacci(int fe ,[int a =0, int b =1]){
+  // base case
+  if(fe<=1){
+    return;
+  }
+  print(a);
+  // recursive fun
+  fibonacci(fe-1,b,a+b);
 }
